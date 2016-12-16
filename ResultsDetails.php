3bb9f83e5d74137aa4b611d0484a3de8867dbc63
klineb <klineb@LX-HFX4GX1.stoneridge.com>
2016-12-16 16:14:31 -0500

    Search

---
+++ ResultsDetails.php
@@ -0,0 +1,37 @@
+<?php
+$race = $_GET['race'];
+$first = $_GET['first'];
+$last = $_GET['last'];
+echo $race;
+echo "<br/>";
+echo $last.", ".$first;
+
+$dbUserName = "raceday_ohio";
+$dbServer = "localhost";
+$dbName = "raceday_ohioraceday";
+$dbPassword = "dead2013frog";
+
+// Create connection
+$conn = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT OverallPlace, BibNo, FirstName, LastName, Sex, Age, FinishingTime, PacePerMile FROM raceresults where RaceName = '".str_replace("'", "''", $race)
+        ."' and LastName = '".$last."' and FirstName ='".$first."';";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table><tr><td>Place</td><td>Bib #</td><td>Name</td><td>Age</td><td>Sex</td><td width='30%'>Time</td><td>Pace</td></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td align='center'>".$row["OverallPlace"]."</td><td>".$row["BibNo"]."</td><td>".$row["FirstName"]." ".$row["LastName"]."</td><td>".$row["Age"]."</td><td>".$row["Sex"]."</td><td>".$row["FinishingTime"]."</td><td>".$row["PacePerMile"]."</td></tr>";
+        //echo "Bib #: " .$row["BibNo"]. " - Name: " . $row["FirstName"]. " " . $row["LastName"]." ".$row["FinishingTime"]. "<br>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
--- Search.php
+++ Search.php
@@ -1,11 +1,14 @@
+<TITLE>Search</TITLE>
+<link href="CSS/ORD.css" rel="stylesheet" type="text/css"/>
 <!DOCTYPE html>
 <html>
 <body>
 
 <form action="search.php" method="post" enctype="multipart/form-data">
     Enter Search Name:
-    <input type="text" name="search">
-    <input type="submit" value="Upload" name="submit">
+    <input type="text" name="first">
+    <input type="text" name="last">
+    <input type="submit" value="Search" name="submit">
 </form>
 
 </body>
@@ -15,7 +18,6 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        print_r($_POST);
         $dbUserName = "raceday_ohio";
         $dbServer = "localhost";
         $dbName = "raceday_ohioraceday";
@@ -29,15 +31,18 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "select RaceName, LastName, FirstName from raceday_ohioraceday.raceresults where LastName like '%".$_POST['search']."%' order by RaceStart;";
+        $sql = "select RaceName, LastName, FirstName from raceday_ohioraceday.raceresults where FirstName like '%".$_POST['first']."%' and LastName like '%"
+                .$_POST['last']."%' order by RaceStart;";
         $result = $conn->query($sql);
         
         if ($result->num_rows > 0) {
     // output data of each row
-    echo "<table><tr><td>First Name</td><td>Last Name</td><td>Race</td></tr>";
+    echo "<table>";
     while($row = $result->fetch_assoc()) {
 
-        echo "<tr><td align='center'>".$row["FirstName"]."</td><td>".$row["LastName"]."</td><td>".$row["RaceName"]."</td></tr>";
+        //echo "<tr><td align='center'>".$row["FirstName"]."</td><td>".$row["LastName"]."</td><td>".$row["RaceName"]."</td></tr>";
+        echo '<tr><td> <a class="resultsLink" href="./ResultsDetails.php?race='.$row["RaceName"].'&first='.$row["FirstName"].'&last='.$row["LastName"].'">'
+                .$row["LastName"].", ".$row["FirstName"]." - ".$row["RaceName"].'</a> </td></tr>';
         //echo "Bib #: " .$row["BibNo"]. " - Name: " . $row["FirstName"]. " " . $row["LastName"]." ".$row["FinishingTime"]. "<br>";
     }
     echo "</table>";

0c134a869e87c6e8c0586406304c5a97dbc739f8
klineb <klineb@LX-HFX4GX1.stoneridge.com>
2017-01-20 15:51:29 -0500

    Results

---
+++ Results.php
@@ -0,0 +1,236 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="http://ohioraceday.com/xmlrpc.php" />
+        <link rel="stylesheet" type="text/css" media="all" href="http://ohioraceday.com/wp-content/themes/poloray/style.css" />         
+        <title>Results &#8211; Ohio Race Day</title>
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="Ohio Race Day &raquo; Feed" href="http://ohioraceday.com/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Ohio Race Day &raquo; Comments Feed" href="http://ohioraceday.com/comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/ohioraceday.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.7.1"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),!(j.toDataURL().length<3e3)&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,65039,8205,55356,57096),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,55356,57096),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55357,56425,55356,57341,8205,55357,56507),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55357,56425,55356,57341,55357,56507),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<script type='text/javascript' src='http://ohioraceday.com/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/ddsmoothmenu.js?ver=4.7.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/cufon-yui.js?ver=4.7.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/Champagne.font.js?ver=4.7.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/jquery-flexslider-min.js?ver=4.7.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/jquery.validate.min.js?ver=4.7.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/custom.js?ver=4.7.1'></script>
+<link rel='https://api.w.org/' href='http://ohioraceday.com/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://ohioraceday.com/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://ohioraceday.com/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.7.1" />
+<link rel="canonical" href="http://ohioraceday.com/results/" />
+<link rel='shortlink' href='http://ohioraceday.com/?p=16' />
+<link rel="alternate" type="application/json+oembed" href="http://ohioraceday.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fohioraceday.com%2Fresults%2F" />
+<link rel="alternate" type="text/xml+oembed" href="http://ohioraceday.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fohioraceday.com%2Fresults%2F&#038;format=xml" />
+<!-- <meta name="NextGEN" version="2.1.69" /> -->
+<link rel="shortcut icon" href="http://ohioraceday.com/wp-content/uploads/2013/01/favicon-5.ico"/>
+    </head>
+    <body class="page-template-default page page-id-16" >
+        <div class="wrapper">
+            <div class="container_24">
+                <div class="grid_24">
+                    <div class="header">
+                        <div class="grid_8 alpha">
+                            <div class="logo">
+                                                                    <a href="http://ohioraceday.com"><img src="http://ohioraceday.com/wp-content/uploads/2012/12/OhioRaceDayTimingtranparent.png" alt="Ohio Race Day" alt="logo"/></a>
+                                                            </div>
+                        </div>
+                        <div class="grid_16 omega">
+                            <div class="menu_container">
+                                <div class="menu_bar">
+                                    <div id="MainNav">
+                                        <a href="#" class="mobile_nav closed">Pages Navigation Menu<span></span></a>
+                                            <div id="menu">
+        <ul class="ddsmoothmenu">
+            <li><a href="http://ohioraceday.com/">Home</a></li><li class="page_item page-item-14 page_item_has_children"><a href="http://ohioraceday.com/races/">Races</a>
+<ul class='children'>
+	<li class="page_item page-item-2609"><a href="http://ohioraceday.com/races/blizzard-busters-5k/">Blizzard Busters 5K</a></li>
+	<li class="page_item page-item-166"><a href="http://ohioraceday.com/races/mardi-gras-5k/">Mardi Gras 5K</a></li>
+	<li class="page_item page-item-838"><a href="http://ohioraceday.com/races/malawi-5k/">Liwiro la Malawi 5K</a></li>
+	<li class="page_item page-item-2089"><a href="http://ohioraceday.com/races/bunny-buster-5k/">Ashland YMCA Bunny Buster 5K</a></li>
+	<li class="page_item page-item-2599"><a href="http://ohioraceday.com/races/runyan/">1LT Michael L Runyan 5K</a></li>
+	<li class="page_item page-item-177"><a href="http://ohioraceday.com/races/kingwood-center-5k/">Kingwood Center 5K</a></li>
+</ul>
+</li>
+<li class="page_item page-item-16 current_page_item"><a href="http://ohioraceday.com/results/">Results</a></li>
+<li class="page_item page-item-1508"><a href="http://ohioraceday.com/archives/">Archives</a></li>
+<li class="page_item page-item-63"><a href="http://ohioraceday.com/photos/">Photos</a></li>
+<li class="page_item page-item-4"><a href="http://ohioraceday.com/contact-us/">Contact Us</a></li>
+        </ul>
+    </div>
+     
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>  
+<div class="heading_container">
+    <div class="container_24">
+        <div class="grid_24">
+            <div class="page-heading">
+                <h1>Results</h1>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div class="page_container">
+    <div class="container_24">
+        <div class="grid_24 body_color">
+            <div class="page-content">
+                <div class="grid_16 alpha">
+                    <div class="content-bar">  			
+                                                    <h4>&nbsp;</h4>
+<h4>&nbsp;<strong><span style="text-decoration: underline;">2016</span></strong></h4>
+<?php
+$dbUserName = "raceday_ohio";
+$dbServer = "localhost";
+$dbName = "raceday_ohioraceday";
+$dbPassword = "dead2013frog";
+//Get list of races
+$connection = new mysqli($dbServer, $dbUserName, $dbPassword);
+    $query = "select `races`.`RaceName`, `races`.`RaceID` FROM `raceday_ohioraceday`.`races` order by left(RaceStart, 4) asc, RaceStart, SortOrder;";
+$results = $connection->query($query);
+if($results->num_rows > 0)
+{
+    echo "<table>";
+    $previousRow = "none";
+    while($singleRow = $results->fetch_assoc())
+    {
+        
+        //echo "<tr><td>".$singleRow["RaceName"]."</td></tr>";
+        if(substr($singleRow["RaceName"], 0, 8) == $previousRow){
+            //echo "<tr><td><hr/></td></tr>";    
+        }
+        else{
+            echo "<tr><td></td></tr>";
+            echo "<tr><td></td></tr>";
+        }
+        echo '<tr><td> <a class="resultsLink" href="./DisplayResults.php?race='.$singleRow["RaceID"].'">'.$singleRow["RaceName"].'</a> </td></tr>';
+        
+        $previousRow = substr($singleRow["RaceName"], 0, 8);
+        
+        //echo var_dump($previousRow);
+    }
+    echo "</table>";
+}
+else
+    echo "Problem";
+?>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><span style="color: #808080;"><strong><a style="color: #808080;" title="results archives" href="http://ohioraceday.com/results/archives/" target="_blank">[CLICK HERE FOR ARCHIVES OF PRIOR YEAR RESULTS]</a></strong></span></p>
+<p>&nbsp;</p>
+                            <div class="clear"></div>
+                                                        <!--Start Comment box-->
+                            <!-- You can start editing here. -->
+<div id="commentsbox">
+                    </div>
+                            <!--End Comment box-->
+                        	
+                    </div>
+                </div>
+                <div class="grid_8 omega">
+                    <!--Start Sidebar-->
+                    <div class="sidebar">
+    			<div class="textwidget"></div>
+		            			<div class="textwidget"></div>
+		          
+</div>                    <!--End Sidebar-->
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div class="footer_wrapper_top">
+    <div class="footer_top_linestrip"></div>
+    <div class="container_24">
+        <div class="grid_24">
+            <div class="footer">
+                <div class="grid_6 alpha">
+    <div class="footer_widget first">
+                    			<div class="textwidget"><p><!-- // MAILCHIMP SUBSCRIBE CODE \\ --><br />
+<a href="http://eepurl.com/vWVvv"><b>CLICK HERE to subscribe<br />
+to our newsletter! <!-- \\ MAILCHIMP SUBSCRIBE LINK // --></p>
+</div>
+		            </div>
+</div>
+<div class="grid_6">
+    <div class="footer_widget">
+                    			<div class="textwidget"></div>
+		          
+    </div>
+</div>
+<div class="grid_6">
+    <div class="footer_widget">
+                    			<div class="textwidget"></div>
+		            </div>
+</div>             			
+<div class="grid_6 omega">
+    <div class="footer_widget last">
+                    			<div class="textwidget"></div>
+		            </div>
+</div>            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div class="footer_wrapper_bottom">
+    <div class="container_24">
+        <div class="grid_24">
+            <div class="grid_16 alpha">
+                <div class="copyright_left">
+                    <a class="tagline" href="http://ohioraceday.com">Ohio Race Day -
+                        Dependable &#8211; Low Cost &#8211; Timing Solutions</a>
+                    <ul class="social_logos">
+                                                    <li><a href="https://www.facebook.com/OhioRaceDay" title="Facebook"><img src="http://ohioraceday.com/wp-content/themes/poloray/images/facebook.png" alt="Facebook" /></a></li>
+                         
+                                                    <li><a href="#" title="Twitter"><img src="http://ohioraceday.com/wp-content/themes/poloray/images/twitter.png" alt="Twitter" /></a> </li>
+                          
+                                                    <li><a href="#" title="Yahoo"><img src="http://ohioraceday.com/wp-content/themes/poloray/images/yahoo.png" alt="Yahoo" /></a></li>
+                                                                            <li><a href="#" title="Rss Feed"><img src="http://ohioraceday.com/wp-content/themes/poloray/images/rss.png" alt="Digg Icon" /></a></li>
+                                                                            <li><a href="#" title="Digg"><img src="http://ohioraceday.com/wp-content/themes/poloray/images/digg.png" alt="Digg icon" /></a></li>
+                                                                            <li><a href="#" title="Pinterest"><img src="http://ohioraceday.com/wp-content/themes/poloray/images/pinterest.png" alt="Pinterest icon" /></a></li>
+                                            </ul>
+                </div>
+            </div>
+            <div class="grid_8 omega">
+                <div class="copyrightinfo">
+                                            <p class="copyright"><a href="http://www.inkthemes.com">Poloray Theme</a> Powered By <a href="http://www.wordpress.org">WordPress</a></p>
+                                    </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<!-- ngg_resource_manager_marker --><script type='text/javascript' src='http://ohioraceday.com/wp-content/themes/poloray/js/mobile-menu.js?ver=4.7.1'></script>
+<script type='text/javascript' src='http://ohioraceday.com/wp-includes/js/wp-embed.min.js?ver=4.7.1'></script>
+</body>
+</html>
